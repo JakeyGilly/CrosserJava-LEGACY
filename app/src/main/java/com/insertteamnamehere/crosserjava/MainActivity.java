df41a055f1c2c31390152cd3410fdb1ae27d3e44
jakeg <jakegillman3@gmail.com>
2020-04-26 14:19:19 +0100

    Bluetooth on added

--- app/src/main/java/com/insertteamnamehere/crosserjava/MainActivity.java
+++ app/src/main/java/com/insertteamnamehere/crosserjava/MainActivity.java
@@ -2,13 +2,31 @@ package com.insertteamnamehere.crosserjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "CrosserLog";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Log.d(TAG, "Bluetooth is not supported on this device");
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+
     }
 }
